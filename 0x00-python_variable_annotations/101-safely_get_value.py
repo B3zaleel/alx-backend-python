d7865fbfb852dbc5a3e109b1b3b5d191624555aa
Bezaleel Olakunori <bezaleeloci@gmail.com>
2022-04-14 10:37:32 +0100

    :adhesive_bandage: Fix styling issues

--- 0x00-python_variable_annotations/101-safely_get_value.py
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -5,12 +5,11 @@ from typing import Any, Mapping, Union, TypeVar
 
 
 T = TypeVar('T')
+Res = Union[Any, T]
+Def = Union[T, None]
 
 
-def safely_get_value(
-    dct: Mapping,
-    key: Any,
-    default: Union[T, None]=None) -> Union[Any, T]:
+def safely_get_value(dct: Mapping, key: Any, default: Def=None) -> Res:
     '''Retrieves a value from a dict using a given key.
     '''
     if key in dct:
