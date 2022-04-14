0643456343c6c7caada4691a4ca527455d0f58ba
Bezaleel Olakunori <bezaleeloci@gmail.com>
2022-04-14 10:50:37 +0100

    :adhesive_bandage: Fix styling issues

--- 0x00-python_variable_annotations/101-safely_get_value.py
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -9,7 +9,7 @@ Res = Union[Any, T]
 Def = Union[T, None]
 
 
-def safely_get_value(dct: Mapping, key: Any, default: Def=None) -> Res:
+def safely_get_value(dct: Mapping, key: Any, default: Def = None) -> Res:
     '''Retrieves a value from a dict using a given key.
     '''
     if key in dct:
