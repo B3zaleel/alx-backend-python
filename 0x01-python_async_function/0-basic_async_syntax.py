2ff5bfb3c142c62962038c85c9e514b8f80eeb2f
Bezaleel Olakunori <bezaleeloci@gmail.com>
2022-04-15 20:17:39 +0100

    :wrench: Change time.sleep to asyncio.sleep

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python3
 '''Task 0's module.
 '''
+import asyncio
 import random
-import time
 
 
 async def wait_random(max_delay: int = 10) -> float:
     '''Waits for a random number of seconds.
     '''
     wait_time = random.random() * max_delay
-    time.sleep(wait_time)
+    await asyncio.sleep(wait_time)
     return wait_time
