13455728899822b3a7b23671ea6fd45dfb19cff3
Bezaleel Olakunori <bezaleeloci@gmail.com>
2022-04-15 20:00:03 +0100

    Revert ":adhesive_bandage: Change native coroutine to generator-based coroutine"
    This reverts commit e2aa110b11943c2c4e35b4980b823be6d02d447c.

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -1,13 +1,11 @@
 #!/usr/bin/env python3
 '''Task 0's module.
 '''
-import asyncio
 import random
 import time
 
 
-@asyncio.coroutine
-def wait_random(max_delay: int = 10) -> float:
+async def wait_random(max_delay: int = 10) -> float:
     '''Waits for a random number of seconds.
     '''
     wait_time = random.random() * max_delay
