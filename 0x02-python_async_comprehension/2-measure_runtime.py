8212465ec1321fd7efb0c0c1918b1cae66626f31
Bezaleel Olakunori <bezaleeloci@gmail.com>
2022-04-19 11:12:59 +0100

    :wrench: Refactor script

--- 0x02-python_async_comprehension/2-measure_runtime.py
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -15,9 +15,6 @@ async def measure_runtime() -> float:
     '''
     start_time = time.time()
     await asyncio.gather(
-        async_comprehension(),
-        async_comprehension(),
-        async_comprehension(),
-        async_comprehension(),
+        *[async_comprehension() for _ in range(4)],
     )
     return time.time() - start_time
