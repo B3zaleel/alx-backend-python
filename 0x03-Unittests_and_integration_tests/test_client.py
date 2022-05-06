33ee3e59471ce839876b03588087f61920a87197
Bezaleel Olakunori <bezaleeloci@gmail.com>
2022-05-06 18:38:20 +0100

    :wrench: Refactor module

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -106,8 +106,8 @@ class TestGithubOrgClient(unittest.TestCase):
         mock_get_json.assert_called_once()
 
     @parameterized.expand([
-        ({"license": {"key": "my_license"}}, "my_license", True),
-        ({"license": {"key": "other_license"}}, "my_license", False),
+        ({'license': {'key': "my_license"}}, "my_license", True),
+        ({'license': {'key': "other_license"}}, "my_license", False),
     ])
     def test_has_license(
             self,
